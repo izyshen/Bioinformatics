21246b6cbcc853b35fa9bdc367823b47e3fa1604
Iris Shen <izyshen@users.noreply.github.com>
2016-12-05 23:59:04 -0500

    Cannot produce first mRNA only

--- index.py
+++ index.py
@@ -32,53 +32,55 @@ first_mRNA_excess = SFTPA1[(SFTPA1.find(METcodon)):]
 first_mRNA = first_mRNA_excess[:(first_mRNA_excess.find(stopcodon1 or stopcodon2 or stopcodon3))]
 while position < len(first_mRNA) - 2 and first_mRNA[position:(position+3)] != (stopcodon1 or stopcodon2 or stopcodon3) :
     print (first_mRNA[position:(position+3)])
+    if first_mRNA[position:(position+3)] == "TTT" or "TTC":
+        print ("Phe")
+    if first_mRNA[position:(position+3)] == "TTA" or "TTG" or "CTT" or "CTC" or "CTA" or "CTG":
+        print ("Leu")
+    if first_mRNA[position:(position+3)] == "ATT" or "ATC" or "ATA":
+        print ("Ile")
+    if first_mRNA[position:(position+3)] == "ATG":
+        print ("START")
+    if first_mRNA[position:(position+3)] == "GTT" or "GTC" or "GTA" or "GTG":
+        print ("Val")
+    if first_mRNA[position:(position+3)] == "TCT" or "TCC" or "TCA" or "TCG" or "AGT" or "AGC":
+        print ("Ser")
+    if first_mRNA[position:(position+3)] == "CCT" or "CCC" or "CCA" or "CCG":
+        print ("Pro")
+    if first_mRNA[position:(position+3)] == "ACT" or "ACC" or "ACA" or "ACG":
+        print ("Thr")
+    if first_mRNA[position:(position+3)] == "GCT" or "GCC" or "GCA" or "GCG":
+        print ("Ala")
+    if first_mRNA[position:(position+3)] == "TAT" or "TAC":
+        print ("Tyr")
+    if first_mRNA[position:(position+3)] == "CAT" or "CAC":
+        print ("His")
+    if first_mRNA[position:(position+3)] == "CAA" or "CAG":
+        print ("Gln")
+    if first_mRNA[position:(position+3)] == "AAT" or "AAC":
+        print ("Asn")
+    if first_mRNA[position:(position+3)] == "AAA" or "AAG":
+        print ("Lys")
+    if first_mRNA[position:(position+3)] == "GAT" or "GAC":
+        print ("Asp")
+    if first_mRNA[position:(position+3)] == "GAA" or "GAG":
+        print ("Glu")
+    if first_mRNA[position:(position+3)] == "TGT" or "TGC":
+        print ("Cys")
+    if first_mRNA[position:(position+3)] == "TGG":
+        print ("Trp")
+    if first_mRNA[position:(position+3)] == "CGT" or "CGC" or "CGA" or "CGG" or "AGA" or "AGG":
+        print ("Arg")
+    if first_mRNA[position:(position+3)] == "GGT" or "GGC" or "GGA" or "GGG":
+        print ("Gly")
+    if first_mRNA[position:(position+3)] == "TAA" or "TAG" or "UGA":
+        print ("STOP")
     position = position + 3
-    
-if first_mRNA[position:(position+3)] == "TTT" or "TTC":
-    print ("Phe")
-if first_mRNA[position:(position+3)] == "TTA" or "TTG" or "CTT" or "CTC" or "CTA" or "CTG":
-    print ("Leu")
-if first_mRNA[position:(position+3)] == "ATT" or "ATC" or "ATA":
-    print ("Ile")
-if first_mRNA[position:(position+3)] == "ATG":
-    print ("START")
-if first_mRNA[position:(position+3)] == "GTT" or "GTC" or "GTA" or "GTG":
-    print ("Val")
-if first_mRNA[position:(position+3)] == "TCT" or "TCC" or "TCA" or "TCG" or "AGT" or "AGC":
-    print ("Ser")
-if first_mRNA[position:(position+3)] == "CCT" or "CCC" or "CCA" or "CCG":
-    print ("Pro")
-if first_mRNA[position:(position+3)] == "ACT" or "ACC" or "ACA" or "ACG":
-    print ("Thr")
-if first_mRNA[position:(position+3)] == "GCT" or "GCC" or "GCA" or "GCG":
-    print ("Ala")
-if first_mRNA[position:(position+3)] == "TAT" or "TAC":
-    print ("Tyr")
-if first_mRNA[position:(position+3)] == "CAT" or "CAC":
-    print ("His")
-if first_mRNA[position:(position+3)] == "CAA" or "CAG":
-    print ("Gln")
-if first_mRNA[position:(position+3)] == "AAT" or "AAC":
-    print ("Asn")
-if first_mRNA[position:(position+3)] == "AAA" or "AAG":
-    print ("Lys")
-if first_mRNA[position:(position+3)] == "GAT" or "GAC":
-    print ("Asp")
-if first_mRNA[position:(position+3)] == "GAA" or "GAG":
-    print ("Glu")
-if first_mRNA[position:(position+3)] == "TGT" or "TGC":
-    print ("Cys")
-if first_mRNA[position:(position+3)] == "TGG":
-    print ("Trp")
-if first_mRNA[position:(position+3)] == "CGT" or "CGC" or "CGA" or "CGG" or "AGA" or "AGG":
-    print ("Arg")
-if first_mRNA[position:(position+3)] == "GGT" or "GGC" or "GGA" or "GGG":
-    print ("Gly")
-if first_mRNA[position:(position+3)] == "TAA" or "TAG" or "UGA":
-    print ("STOP")
         
-
-
+##number_AA = ((len(first_mRNA))/3)
+##aa1 = (first_mRNA[0:3])
+##aa2 = (first_mRNA[3:6])
+##print (aa1)
+##print (aa2)
 
 
 # n.remove(item)
