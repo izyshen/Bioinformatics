d1b00c5a5e8804d97bb11058f0a78d5bed7f7e0d
Iris Shen <izyshen@users.noreply.github.com>
2016-12-13 00:33:48 -0500

    Produces mRNA, need stop codon

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 print ("Test Page")
 
-DNAseq = "ATGCGTATTGACCTTTTACTGTACTAA"
+DNA = "ATGCAACGTATTGACCTTTTACTGTACTAA"
 
-position = 0
+##position = 0
 
-while position < len(DNAseq) - 2:
-    if DNAseq[position:(position + 3)] == "ATG":
-        print "START"
-        position = position + 3
-    if DNAseq[position:(position + 3)] == "CGT" or DNAseq[position:(position + 3)] == "ATT":
-        print "Arg"
-        position = position + 3
-    else: 
-        print DNAseq[position:(position + 3)]
-        position = position + 3
+##while position < len(DNAseq) - 2:
+##    if DNAseq[position:(position + 3)] == "ATG":
+##        print "START"
+##        position = position + 3
+##    if DNAseq[position:(position + 3)] == "CGT" or DNAseq[position:(position + 3)] == "ATT":
+##        print "Arg"
+##        position = position + 3
+##    else: 
+##        print DNAseq[position:(position + 3)]
+##        position = position + 3
     
+def mRNA_producer (DNAseq, position):
+    while position < len(DNAseq) - 2:
+        if DNAseq[position:(position + 3)] == "ATG":
+            print DNAseq[position:]
+            position = len(DNAseq)
+        if position == len(DNAseq) - 3 and DNAseq[position:] != "ATG":
+            print "No ORF"
+            position = len(DNAseq)
+        else: position = position + 1
+                             
