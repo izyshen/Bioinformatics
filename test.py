e3963c174fee9ac63e4e2e751fc8a3738f6c35df
Iris Shen <izyshen@users.noreply.github.com>
2016-12-13 00:53:52 -0500

    Success! MRNA produced!

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ print ("Test Page")
 
 DNA = "ATGCAACGTATTGACCTTTTACTGTACTAA"
 
+# premature stop codon added in + DNAseq not divisible by 3
+DNA2 = "CATGCAACGTATTGACCTTTTAAAAAAAAAAAAAAAAAAAAAAAAAACTGTACTATAASG"
+
+METcodon = "ATG"
+stopcodon1 = "TAA"
+stopcodon2 = "TAG"
+stopcodon3 = "TGA"
 ##position = 0
 
 ##while position < len(DNAseq) - 2:
@@ -14,14 +21,19 @@ DNA = "ATGCAACGTATTGACCTTTTACTGTACTAA"
 ##    else: 
 ##        print DNAseq[position:(position + 3)]
 ##        position = position + 3
-    
+
+## requires function to have a stopcodon in frame to terminate    
 def mRNA_producer (DNAseq, position):
     while position < len(DNAseq) - 2:
-        if DNAseq[position:(position + 3)] == "ATG":
-            print DNAseq[position:]
+        if DNAseq[position:(position + 3)] == METcodon:
+            while position < len(DNAseq):
+                if DNAseq[position:(position + 3)] == (stopcodon1 or stopcodon2 or stopcodon3):
+                    print "STOP"
+                    position = len(DNAseq)
+                else: print DNAseq[position:(position + 3)]
+                position = position + 3
             position = len(DNAseq)
-        if position == len(DNAseq) - 3 and DNAseq[position:] != "ATG":
+        if position == len(DNAseq) - 3 and DNAseq[position:] != METcodon:
             print "No ORF"
             position = len(DNAseq)
         else: position = position + 1
-                             
